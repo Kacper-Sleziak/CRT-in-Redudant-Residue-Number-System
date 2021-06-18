6e9fd71037707931e727e3c1d46721f324242e60
Kacper <kacper-sleziak@wp.pl>
2021-06-18 22:20:41 +0200

    Test_Next_Update

--- Rank_Test.py
+++ Rank_Test.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# funkcja sprawdzająca czy dwie liczby są wzglednie pierwsze
+
 def are_coprime(a, b):
     hcf = 1
 
@@ -15,18 +17,19 @@ def are_coprime(a, b):
     return hcf == 1
 
 
-pos_number = 1000000                                    # liczba, ktorej range bedziemy obliczac
-number = int(math.pow(2, 13))                           # liczba od ktorej zaczynamy wyszukiwanie liczb wzglednie pierwszych
+pos_number = 1000000                                        # liczba, ktorej range bedziemy obliczac
+number = int(math.pow(2, 13))                               # liczba od ktorej zaczynamy wyszukiwanie liczb wzglednie pierwszych
 
-coprime_list = []                                       # lista liczb pierwszych i rownoczesnia najwieksza baza
-time_list = np.array([])                                # tablica usrednionych pomiarow
-base_list = []                                          # lista baz
-base_lengths = np.array([])                             # tablica wielkosci baz
+coprime_list = []                                           # lista liczb pierwszych i rownoczesnia najwieksza baza
+time_list_redudant = np.array([])                           # tablica usrednionych pomiarow z nadmiarem
+time_list_non_redudant = np.array([])                       # tablica usrednionych pomiarow bez nadmiarow
+base_list = []                                              # lista baz
+base_lengths = np.array([])                                 # tablica wielkosci baz
 
-number_of_tests = 1000                                   # liczba testow do przeprowadzenia
-largest_base = 1000                                     # wielkosc najwiekszej bazy
-number_of_bases = 20                                    # liczba baz dla, ktorej chcemy dokonac pomiarow
-density_of_diagram = largest_base // number_of_bases    # liczba punktow na wykresie
+number_of_tests = 1000                                      # liczba testow do przeprowadzenia
+largest_base = 1000                                         # wielkosc najwiekszej bazy
+number_of_bases = 20                                        # liczba baz dla, ktorej chcemy dokonac pomiarow
+density_of_diagram = largest_base // number_of_bases        # liczba punktow na wykresie
 
 counter = 0
 
@@ -68,25 +71,36 @@ for i in range(number_of_bases):
 
 # wlasciwe pomiary czasu
 for i in range(number_of_bases):
-    time_of_operation = 0
+    time_of_operation_redudant = 0
+    time_of_operation_non_redudant = 0
 
     for k in range(number_of_tests):
         rns = System(base_list[i], pos_number)
-        start_time = time.time()
+
+        start_time_redudant = time.time()
         rns.get_rank_of_number_redundant()
+        time_of_operation_redudant = time_of_operation_redudant + (time.time() - start_time_redudant) * 10 ** 3
 
-        time_of_operation = time_of_operation + (time.time() - start_time) * 10 ** 3
+        start_timne_non_redudant = time.time()
+        rns.get_rank_of_number()
+        time_of_operation_non_redudant = time_of_operation_non_redudant + (time.time() - start_time_redudant) * 10 ** 3
 
-    average_time_of_operation = time_of_operation / number_of_tests
+    average_time_of_operation_redudant = time_of_operation_redudant / number_of_tests
+    average_time_of_operation_non_redudant = time_of_operation_non_redudant / number_of_tests
 
-    time_list = np.append(time_list, average_time_of_operation)
+    time_list_redudant = np.append(time_list_redudant, average_time_of_operation_redudant)
+    time_list_non_redudant = np.append(time_list_non_redudant, abs(average_time_of_operation_non_redudant))
 
-y = time_list
+y_1 = time_list_redudant
 x = base_lengths
-
-plt.plot(x, y)
+y_2 = time_list_non_redudant
 
 plt.xlabel("Liczba modulow w bazie")
 plt.ylabel("Czas obliczania rangi [ms]")
+
+plt.plot(x, y_1, label = "Ranga z nadmiarem")
+plt.plot(x, y_2, label = "Ranga bez nadmiaru")
+
+plt.legend()
 
 plt.show()
